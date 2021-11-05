13adce92244153d9e932c1e84250215e472b864e
formatspecifiernotfound <danxie2001@gmail.com>
2021-11-05 14:55:59 -0400

    pyqt5 implementation

--- testcase.py
+++ testcase.py
@@ -1,67 +1,81 @@
-import tkinter as tk
-from tkinter.filedialog import askopenfilename
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
 import matlab.engine
 import numpy as np
 import scipy.io.wavfile as wav
-from matplotlib.backends.backend_tkagg import (
-    FigureCanvasTkAgg, NavigationToolbar2Tk)
-from matplotlib.backend_bases import key_press_handler
-from matplotlib.figure import Figure
+import sys
 
 eng1 = matlab.engine.start_matlab()
 
-root = tk.Tk()
-root.wm_title("Sound Generator and Editor")
+class MainWindow(QtWidgets.QMainWindow):
+
+    filepath = None
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("Generator and Editor")
+
+        self.graphWidget = pg.PlotWidget()
+        self.setCentralWidget(self.graphWidget)
+
+        self.defineToolbar()
+
+        
+
+        # plot data: x, y values
+        #self.graphWidget.plot(hour, temperature)
+
+    def plottingFig(self, y, Fs, tt, graphWidget):
+        y = np.asarray(y, dtype=np.float32)
+        y = y.reshape(len(y),)
+        t = np.arange(0, tt, 1/Fs)
+        graphWidget.plot(t, y)
+
+    def defineToolbar(self):
+        self.toolbar = QtWidgets.QToolBar()
+        self.addToolBar(self.toolbar)
+
+        fileButton = QAction("Browse", self)
+        fileButton.setShortcut('Ctrl+O')
+        fileButton.triggered.connect(self.getFiles)
+        self.toolbar.addAction(fileButton)
+
+        #graphButton = QAction("Graph", self)
+        #graphButton.setShortcut('Ctrl+O')
+        #graphButton.triggered.connect(self.plottingFig(self.graphWidget))
+        #self.toolbar.addAction(graphButton)
+
+        #print("filepath:", self.filepath)
+
+    def getFiles(self):
+        filepath, _ = QtWidgets.QFileDialog.getOpenFileName(
+            self, 'Single File', QtCore.QDir.rootPath(), '*.wav')
+        self.readFile(filepath)
+
+    def readFile(self, filepath):
+        #Fs, y = wav.read(filepath)
+        #y = y / 32768.0
+        #tt = len(y)/float(Fs)
+        y, Fs, tt = eng1.tGraph(filepath, nargout=3)
+        self.plottingFig(y, Fs, tt, self.graphWidget)
+
+
+def main():
+    # playSound(filepath)
+
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
 
 def playSound(eng1):
     eng1.playSound(nargout=0)
 
-def plottingFig(y, Fs, tt):
-    fig = Figure(figsize=(5, 4), dpi=100)
-    t = np.arange(0, tt, 1/Fs)
-    fig.add_subplot().plot(t, y)
-    return fig
 
-def disp_toolbar(canvas):
-    toolbar = NavigationToolbar2Tk(canvas, root, pack_toolbar=False)
-    toolbar.update()
-
-    canvas.mpl_connect(
-        "key_press_event", lambda event: print(f"you pressed {event.key}"))
-    canvas.mpl_connect("key_press_event", key_press_handler)
-    return toolbar
-
-def readFile(filepath): #optional read using python
-    Fs, y = wav.read(filepath)
-    y = y / 32768.0
-    tt = len(y)/float(Fs)
-    return y, Fs, tt
-
-def main():
-
-    filepath = askopenfilename()
-    print(filepath)
-
-    playSound()
-
-    y, Fs, tt = readFile(filepath)
-
-    y,Fs,tt = eng1.tGraph(filepath,nargout=3)
-
-    fig = plottingFig(y,Fs,tt)
-
-    canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
-    canvas.draw()
-
-    toolbar = disp_toolbar(canvas)
-
-    toolbar.pack(side=tk.TOP, fill=tk.X)
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-    root.mainloop()
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
 
 eng1.quit()
