7d016facfb2ccc7bf08bca4e512bc34215975107
formatspecifiernotfound <danxie2001@gmail.com>
2021-12-03 12:08:56 -0500

    hopefully this doesnt break anything
    fingers crossed

--- createOutput.m
+++ createOutput.m
@@ -1,16 +1,14 @@
-function [output] = createOutput(type, fs, fName) %params: fs, type, sine freq, stop time, f0, f1
-    
-    if ~exist('fName','var')
-     % third parameter does not exist, so default it to something
-      fName = 'handel.wav';
-    end
+function [output] = createOutput(type, fs, fName, sine_F, f0, f1) %params: fs, type, sine freq, stop time, f0, f1
 
     switch type
         case 0 %tone
+            if ~exist('sine_F','var')
+                sine_F = 5000;
+            end 
             dt = 1/fs;                   % seconds per sample
             StopTime = 0.20;             % seconds
             t = (0:dt:StopTime-dt)';     % seconds
-            F = 5000;                    % Sine wave frequency (hertz)
+            F = sine_F;                    % Sine wave frequency (hertz)
             output = sin(2*pi*F*t);
                 
             %define envelope
@@ -27,14 +25,23 @@ function [output] = createOutput(type, fs, fName) %params: fs, type, sine freq, 
               
             output=output.*env;
         case 1 %chirp
+            if ~exist('f0','var')
+                f0 = 1000;
+            end
+            if ~exist('f0','var')
+                f1 = 20000;
+            end
+
             dt = 1/fs;                   % seconds per sample
             StopTime = 0.10;             % seconds
             t = (0:dt:StopTime-dt)'; 
-            f0 = 1000;
-            f1 = 20000;
             x = chirp(t,f0,t(end),f1);
             output = x,fs;
         case 2
+            if ~exist('fName','var')
+             % third parameter does not exist, so default it to something
+              fName = 'handel.wav';
+            end
             [output, Fs] = audioread(fName);
     end
 end
