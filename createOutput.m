dd0056082fbfa9f510f4bdd95e201762116e8f09
formatspecifiernotfound <danxie2001@gmail.com>
2021-12-07 23:47:52 -0500

    tramp, tsil

--- createOutput.m
+++ createOutput.m
@@ -1,8 +1,16 @@
-function [output] = createOutput(type, fs, StopTime, dtcycle, sine_F, f0, f1) 
+function [output] = createOutput(type, fs, amp, tsil, tramp, StopTime, dtcycle, sine_F, f0, f1) 
     % seconds StopTime
 
-    %main params: type, sampling frequency, amp, t-silence, t-ramp, offset
-    %optional args: duty cycle, pulse width (pulse), f0, f1 (chirp), sine 
+    %main params: type, sampling frequency (khz), amp (v), tsilence (ms),
+    %t-ramp (ms), offset (0 default, in volts)
+    %optional args: duty cycle, f0, f1 (chirp), sine 
+
+    if ~exist('offset','var')
+        offset = 0;
+    end
+    if ~exist('amp','var')
+        amp = 1;
+    end
 
     switch type
         case 0 %pulse
@@ -15,12 +23,10 @@ function [output] = createOutput(type, fs, StopTime, dtcycle, sine_F, f0, f1)
            dt = 1/fs;
        
            t = (0:dt:StopTime-dt)'; 
-           %d = (0:)
-           output = pulstran(t,t,x);
-           plot(t, output);
+           output = pulstran(t,offset,x);
         case 1 %noise
-            lb = -1; %change
-            ub = 1; % 
+            lb = -(amp/2) + offset; %change
+            ub = (amp/2) + offset; % 
             samp = fs*StopTime;
             output = lb + rand(1,samp)*(ub - lb);
             output = rot90(output);
@@ -44,22 +50,36 @@ function [output] = createOutput(type, fs, StopTime, dtcycle, sine_F, f0, f1)
             t = (0:dt:StopTime-dt)';     % seconds
             F = sine_F;                    % Sine wave frequency (hertz)
             output = sin(2*pi*F*t);
-                
-            %define envelope
-            % env = ones(length(tone),1);
-            rep_no=5;
-            rep_sz=length(output)/rep_no;
-            hf_rep = rep_sz/2;
-                
-            env=[];
-            for i = 1:5
-                temp = vertcat(zeros(hf_rep,1), ones(hf_rep,1));
-                env = vertcat(env,temp);   
-            end
-              
-            output=output.*env;
         
         case 4 %periodic/square
             
     end
+
+    %define envelope
+
+    rat = tramp/StopTime;
+    silp = tsil/StopTime;
+
+    sil_rep = ceil(length(output)*silp);
+    ramp_rep = ceil(length(output)*rat);
+
+    rep_sz = length(output) - 2*ramp_rep - 2*sil_rep;
+
+    ramp = 0: 1/(ramp_rep) : 1 - (1/ramp_rep);
+
+    env=[];
+    temp(1:sil_rep) = zeros(sil_rep,1);
+    firstramp = sil_rep+length(ramp);
+    temp(sil_rep+1:firstramp) = ramp;
+    signal_sz = firstramp + rep_sz;
+    temp(firstramp+1:signal_sz) = ones(rep_sz,1);
+    lastramp = signal_sz + length(ramp);
+    temp(signal_sz+1:lastramp) = flip(ramp);
+    temp(lastramp+1:length(output)) = zeros(sil_rep,1);
+
+    temp = rot90(temp);
+    env = vertcat(env,temp);   
+             
+    output=output.*env + offset;
+
 end
--- tGraph.m
+++ tGraph.m
@@ -1,21 +1,20 @@
-function [y,Fs,tt] = tGraph(fName)
+function tGraph()
+    sine_F = 5000;
+    fs = 44000;
+    amp= 2;
+    StopTime = 0.1;
+    offset = 1;
+    dt = 1/fs;                   % seconds per sample
+    t = (0:dt:StopTime-dt)';     % seconds
+    tramp = StopTime/3;
+    tsil = StopTime/9;
+    F = sine_F;                    % Sine wave frequency (hertz)
+    output = amp*sin(2*pi*F*t);
+                
+    %define envelope
+    % env = ones(length(tone),1);
+   
+    output = pulstran(t,offset,'rectpuls',fs);
 
-    [y,Fs] = audioread(fName);
-    
-    info = audioinfo(fName);
-
-    tt = info.Duration;
-
-    t = 0:seconds(1/Fs):seconds(info.Duration);
-    t = t(1:end-1);
-
-    t = 0 : 1/1e6 : .01;         
-    d = 0 : 1/5 : .01           
-    y = pulstran(t,d,@rectpuls);
-    plot(t,y); 
-    axis([0 0.001 0 1.5])
-    
-    %plot(t,y)
-    %xlabel('Time')
-    %ylabel('Audio Signal')
+    plot(t, output)
 end
